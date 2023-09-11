6f5f75331c342870a9ef9b17a68ade205f8cd2e8
ryutaeyeong <tybam@naver.com>
2023-09-11 17:51:21 +0900

    Feat: create UsedProductLike Entity
    Former-commit-id: a695d23ad92eb51cb9be96df5ef490d063c80051

--- backend/ssaguri/src/main/java/com/ssafy/ssaguri/entity/BaseTimeEntity.java
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:f02c2101b9a05dd1ea1a489504fda8ad05643eb0bdc00dbd5f2a0a2c90e1a081
-size 789
--- backend/ssaguri/src/main/java/com/ssafy/ssaguri/entity/usedproduct/UsedProduct.java
+++ backend/ssaguri/src/main/java/com/ssafy/ssaguri/entity/usedproduct/UsedProduct.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:90c66ca16fca7217dcab39eae74f2350b98e9380223d12d9bcef937af8dab386
-size 1520
+oid sha256:494cf69dc809374286262ed4a6206cb704cb80c80dada68b2fc09752f1b02160
+size 1527
