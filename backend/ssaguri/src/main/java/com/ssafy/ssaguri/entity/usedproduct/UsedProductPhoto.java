159f1ca9220272718b7ff68bd689794f033d8097
유태영 <tybam@naver.com>
2023-09-12 15:47:10 +0900

    Feat: create beatboard, bestboardcomment entity
    Former-commit-id: b8583ec6eef3b7fbcaa7b76f33d355ed1af19cdd

--- backend/ssaguri/src/main/java/com/ssafy/ssaguri/entity/usedproduct/UsedProductPhoto.java
+++ backend/ssaguri/src/main/java/com/ssafy/ssaguri/entity/usedproduct/UsedProductPhoto.java
@@ -1,2 +1,3 @@
-package com.ssafy.ssaguri.entity.usedproduct;public class UsedProductPhoto {
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:e227a3b09500cfc76942b48053b7cda5de22212dc2a26a534e39cdaa5a8e81bf
+size 925
