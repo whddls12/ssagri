4a93c61b0055dbb1f4344ea178db6243ce993acd
thdqudgns <thdqudgns9612@gmail.com>
2023-09-18 22:00:58 +0900

    Update: 채팅창 화면 구성 80% 완려
    Former-commit-id: 60b69d0dd562bd6f2d5be1f6488a978e31eb8844

--- frontend/src/components/chattingPage.styles.tsx
+++ frontend/src/components/chattingPage.styles.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e55c6bd6a22cb1e8231d2b5ad72d1a4363c36a104c8e529d6d21c79b3f49eb94
-size 2389
+oid sha256:ad9cc575070ce8858f0b127ed2322b64ae1b5d51e73bb9235f930fdab595c57c
+size 9095
