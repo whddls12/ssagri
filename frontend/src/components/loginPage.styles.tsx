f4edc76661e112ab2e8bf851ad34e1fae1c63b89
황종인 <ghkdwhddls12@daum.net>
2023-09-19 10:14:46 +0900

    Style: 전체 폰트 설정 및 box-sizing(border-box) 적용
    Former-commit-id: a7af84090350e27f35593979522adef2635c0c1a

--- frontend/src/components/loginPage.styles.tsx
+++ frontend/src/components/loginPage.styles.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6222fed9a031a0a29bfd4f92c3f04ae2d4961976853537a73d0d11e9d4af18b3
-size 11812
+oid sha256:996f2abc43785c564f7f557c72067bd49e9631db0f0695b8ad818e8ef1cafb82
+size 11753
