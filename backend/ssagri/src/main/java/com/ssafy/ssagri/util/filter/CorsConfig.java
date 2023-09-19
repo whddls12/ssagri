6b33f4bfb5051e3c253fe447ff3b9b7bc7c5d611
황종인 <ghkdwhddls12@daum.net>
2023-09-19 15:24:14 +0900

    Fix: 백엔드 CORS 수정
    Former-commit-id: 6b11616b8a5407251ff72c46f875d0c4045223eb

--- backend/ssagri/src/main/java/com/ssafy/ssagri/util/filter/CorsConfig.java
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:a35150f75ae13711a473d5c0686bd4e24c9d5633563ae437d95f98b217fd7a3d
-size 1804
--- frontend/src/components/loginPage.styles.tsx
+++ frontend/src/components/loginPage.styles.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6846c0bd10e5bdacff480b40abea71e641eacce866aceec18cf6de8ee98b6d25
-size 13123
+oid sha256:7a1ca9a7c5b4be9745a2992e9e9c96eb30e297057c2b49eda9ffa052f1c8ef0c
+size 17006
