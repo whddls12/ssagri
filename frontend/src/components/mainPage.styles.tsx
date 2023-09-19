c3a43c9beddd3098ba9366591a0c43ea9629827b
석정원 <15gpfk@naver.com>
2023-09-19 10:41:10 +0900

    Update mainPage.styles.tsx
    Former-commit-id: 063300a0c7abda617e31099d63794f1f8deb4d70

--- frontend/src/components/mainPage.styles.tsx
+++ frontend/src/components/mainPage.styles.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6976c8372186fdd21db982c317a70a0fedc8e344564beebaeb15c7c840515559
-size 22935
+oid sha256:22da53da08bfdda158ee2c0286525bdbbb3eca4b8bef94d1a70abf9af28f4372
+size 24690
