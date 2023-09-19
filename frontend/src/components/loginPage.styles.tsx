b175a63e70e1f50d47bf87d5ffbdb895612e7eb2
황종인 <ghkdwhddls12@daum.net>
2023-09-19 09:57:23 +0900

    Feat: 회원가입 이메일 유효성검사 수정중
    Former-commit-id: c2c3e15c5007508443ea994df796b1f68cc79bbc

--- frontend/src/components/loginPage.styles.tsx
+++ frontend/src/components/loginPage.styles.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:85c4c33376edcdee4f2507664651b673b391bbd71e22285d2c03421f5e3419c7
-size 11116
+oid sha256:6222fed9a031a0a29bfd4f92c3f04ae2d4961976853537a73d0d11e9d4af18b3
+size 11812
