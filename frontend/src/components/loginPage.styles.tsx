f32e7283082df2ca499323dc318019f56745695f
황종인 <ghkdwhddls12@daum.net>
2023-09-19 11:12:57 +0900

    Style: 회원가입 필드 배치 수정
    Former-commit-id: 0a71e8497c37beab5f278471f6b21a4bed2af0e2

--- frontend/src/components/loginPage.styles.tsx
+++ frontend/src/components/loginPage.styles.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:996f2abc43785c564f7f557c72067bd49e9631db0f0695b8ad818e8ef1cafb82
-size 11753
+oid sha256:6846c0bd10e5bdacff480b40abea71e641eacce866aceec18cf6de8ee98b6d25
+size 13123
