afdd7ae7ae7c5c9eba5277317cd8fe6f809974c6
thdqudgns <thdqudgns9612@gmail.com>
2023-09-19 15:53:19 +0900

    Update: 채팅화면 구현 완료 및 box-sizing에 따른 px 수정
    Former-commit-id: 23af44e594ed215badc9022e0f7e953c4295134c

--- frontend/src/components/chattingPage.styles.tsx
+++ frontend/src/components/chattingPage.styles.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6754a707d9790fc64b3b1c4c5d283debff6e0d2f48543b0ff336c029394e596e
-size 14450
+oid sha256:45f664b71ebbd509a99d038179bb4ed38103bde230788f72a2df3a3a0f2ea1d0
+size 14456
