bdb20a6542dbb36bd6d0cc0c3ff6368d477d3e3f
황종인 <ghkdwhddls12@daum.net>
2023-09-20 14:00:10 +0900

    Update: 필요없는 부분 제거
    Former-commit-id: 1795ccc5aa9f037b5ee4d096caf4ed04a1e9fc2c

--- frontend/src/components/loginPage.styles.tsx
+++ frontend/src/components/loginPage.styles.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7b5458c55df1479d54a1f50d75b295ca94b67438ebeeab3ac0bf878cf936fad6
-size 19779
+oid sha256:84e7e42f578795e68c6efc6a36b4ee2a08a469d17676b26e3c548228324ba94c
+size 20180
