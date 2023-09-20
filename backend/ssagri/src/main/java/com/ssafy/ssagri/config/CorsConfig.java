bde51e0f40871d9eee18dce7810544ac68cc1235
황종인 <ghkdwhddls12@daum.net>
2023-09-20 13:19:38 +0900

    Feat: CORS 설정 5173 추가
    Former-commit-id: 886db1e5918b1a3dd855464c71e8a69c8831214c

--- backend/ssagri/src/main/java/com/ssafy/ssagri/config/CorsConfig.java
+++ backend/ssagri/src/main/java/com/ssafy/ssagri/config/CorsConfig.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:aff7dc97dd940db2c47c3d96001aa21c84576dfc81cfef23aa13611d168e1804
-size 1824
+oid sha256:b3572d556e3381837fd5b6bf4bf0060a5dee48e9ee14f672b6da5badb9e436b6
+size 1882
