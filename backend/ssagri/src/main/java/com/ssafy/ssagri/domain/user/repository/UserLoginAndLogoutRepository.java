8f92f0ea30ccb11ec1df2c06db3f1b033930c76f
신창학 <csg1353@naver.com>
2023-09-20 14:30:14 +0900

    feat : 로그인 시 토큰 발급" git commit -m "feat : 로그인 시 토큰 발급" "
    Former-commit-id: f7c534c56d48bbbb53215961784f37d1f7edee5b

--- backend/ssagri/src/main/java/com/ssafy/ssagri/domain/user/repository/UserLoginAndLogoutRepository.java
+++ backend/ssagri/src/main/java/com/ssafy/ssagri/domain/user/repository/UserLoginAndLogoutRepository.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5eb114f533cb0add5f1f63cb6c88bc7d3b2b3d728efea8695c228b78512a88c6
-size 726
+oid sha256:87f1c901a80c688f82f96ba9394f3fa64dcd07a2c3ebe156fd00dd8e09dde01b
+size 725
