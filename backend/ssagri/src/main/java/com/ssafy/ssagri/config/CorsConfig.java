67727d0b6298efc226424de2d5a432a3fa21cc91
신창학 <csg1353@naver.com>
2023-09-21 11:04:22 +0900

    feat : Redis 해시값 저장 삭제수정 및 CORS 변경
    Former-commit-id: b4ca9b18d7788ff54be5fec902df77d841e4c0db

--- backend/ssagri/src/main/java/com/ssafy/ssagri/config/CorsConfig.java
+++ backend/ssagri/src/main/java/com/ssafy/ssagri/config/CorsConfig.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:594f00cb2565b6fa2dfc0990e77c018b2a5b582a0190566465a00e988a97b95c
-size 2068
+oid sha256:684a17a2c2fd3d90698a1ec5ded6f88817a4a7c77f4736cf540bca12a4df80d7
+size 2158
--- backend/ssagri/src/main/java/com/ssafy/ssagri/domain/user/service/UserLoginAndLogoutService.java
+++ backend/ssagri/src/main/java/com/ssafy/ssagri/domain/user/service/UserLoginAndLogoutService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8475dbdab6af061124b9627b64e34151b0026e9d757f58889d483a65e9815f93
-size 4945
+oid sha256:eae8f45eae006de04d0165ce4371afea54c61db61bb706429bf68ef39bcc708e
+size 5222
