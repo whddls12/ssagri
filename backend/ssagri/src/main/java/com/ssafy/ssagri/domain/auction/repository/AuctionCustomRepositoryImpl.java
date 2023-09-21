284ae24b6232bef80a2fc6d246cb7ea95de47c22
유태영 <tybam@naver.com>
2023-09-21 16:13:30 +0900

    Update: description
    Former-commit-id: 001c5a401db1560a95561977635b63493f1bcabb

--- backend/ssagri/src/main/java/com/ssafy/ssagri/domain/auction/repository/AuctionCustomRepositoryImpl.java
+++ backend/ssagri/src/main/java/com/ssafy/ssagri/domain/auction/repository/AuctionCustomRepositoryImpl.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:192e43a393d3ce2da6e938451247a77e15643ad69c1ad11586443718170ecd08
-size 916
+oid sha256:88c28b4fe8c6ac8301d889dbace7d571cd3fc27b13b84b35a0ebe1eb26a097a3
+size 886
