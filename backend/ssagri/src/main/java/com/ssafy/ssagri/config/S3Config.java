fb8727ac7da93d95d72ffdcf0ae6ab5c5553fb54
handaldog <handal@kakao.com>
2023-09-21 17:42:12 +0900

    ãfeature: s3 ì생 성
    Former-commit-id: 3d369b9803e54d05dccb4e9e9f8b9313475823c2

---
+++ backend/ssagri/src/main/java/com/ssafy/ssagri/config/S3Config.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:7a2284926440c7c43636c8b83dd2ef60a5651b48640c58155cbf1af12e383235
+size 1230
--- backend/ssagri/src/main/java/com/ssafy/ssagri/domain/auction/service/AuctionProductService.java
+++ backend/ssagri/src/main/java/com/ssafy/ssagri/domain/auction/service/AuctionProductService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1bd10365eb0540616e208af3d2f89adf5e3e548ef6d12382c757b8057e7d70b9
-size 6178
+oid sha256:2fd55a4e116539d1c85288f909c958078bf66e51965977af8070fd9fe3d7cfd4
+size 6333
