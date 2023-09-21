b6ec1ed3f7cbc89a6264f23c3a82a0ec3d2fe7dc
신창학 <csg1353@naver.com>
2023-09-21 16:33:59 +0900

    Refactor : Refresh Token 발급 -> Refresh Cookie 설정으로 변경
    Former-commit-id: 3113469d84d4ef687fccfa4d5e45e89d95bbe659

--- backend/ssagri/src/main/java/com/ssafy/ssagri/domain/user/controller/UserLoginAndLogoutController.java
+++ backend/ssagri/src/main/java/com/ssafy/ssagri/domain/user/controller/UserLoginAndLogoutController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8131da82fb994993d240538352766c996a971ad464887369fa6f3682c0d5f25f
-size 2761
+oid sha256:c98f67efbec226089275702351919efd10e101a1b59f5afffffe83739a7cbf27
+size 2848
