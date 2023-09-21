a001f24f22cebda8916074a4d8c01355d1b09a62
유태영 <tybam@naver.com>
2023-09-21 17:25:12 +0900

    Update: usedproduct select booleanbuilder
    Former-commit-id: 5e43d01a051346a89690ebad9c5674551274a21b

--- backend/ssagri/src/main/java/com/ssafy/ssagri/domain/usedproduct/service/UsedProductService.java
+++ backend/ssagri/src/main/java/com/ssafy/ssagri/domain/usedproduct/service/UsedProductService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:db601fbe57cbdfef386aa057a88bfdc8f79181f3aa9423d90832cf710feed62e
-size 7935
+oid sha256:0d3212befece5d665b83f23ee08e00fc742929d5a64772eadb8b442df2fc2cbb
+size 8005
