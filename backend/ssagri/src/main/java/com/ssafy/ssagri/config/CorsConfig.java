025ef30dea8f68c1bc89559e7b0ae0ab6049a1ba
유태영 <tybam@naver.com>
2023-09-21 10:01:10 +0900

    Feat: setting
    Former-commit-id: 188e01a2cf4ea999f1e8d4cd80e0d472d2c79100

--- backend/ssagri/src/main/java/com/ssafy/ssagri/config/CorsConfig.java
+++ backend/ssagri/src/main/java/com/ssafy/ssagri/config/CorsConfig.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:594f00cb2565b6fa2dfc0990e77c018b2a5b582a0190566465a00e988a97b95c
-size 2068
+oid sha256:6f495785453dd4fc123e3860a6cf3a6f209e512642c63d4f64881bfced2829cb
+size 2091
