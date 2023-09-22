f415c57b7daf31e5b5e65fc2dd8a7482b523cd41
신창학 <csg1353@naver.com>
2023-09-22 17:56:08 +0900

    feat : 중간저장, 랜덤닉네임 생성 중 중단
    Former-commit-id: 154dab84752bcfe034cd48e93c780faeb48a15fb

---
+++ backend/ssagri/src/main/java/com/ssafy/ssagri/domain/user/repository/RandomFirstNameRepository.java
@@ -0,0 +1,10 @@
+package com.ssafy.ssagri.domain.user.repository;
+
+import com.ssafy.ssagri.entity.user.RandomNicknameFirst;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+public interface RandomFirstNameRepository extends JpaRepository<RandomNicknameFirst, Long> {
+    @Query(value = "SELECT firstName FROM random_nickname_first ORDER BY RAND() LIMIT 1", nativeQuery = true)
+    String getRandomName();
+}
