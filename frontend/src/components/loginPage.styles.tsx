47a295a613020a591fc05c3ae8e16cd46af9a28c
황종인 <ghkdwhddls12@daum.net>
2023-09-25 10:50:34 +0900

    fix: 이메일 인증요청 방식 수정 post -> get
    Former-commit-id: 11408a2bc35aa64f7e27e9655596b8e814242422

--- frontend/src/components/loginPage.styles.tsx
+++ frontend/src/components/loginPage.styles.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a7a6b6c12b4e6935c4f9610085b62d00ed285a9affae51eb20682c8c8f0ee075
-size 24781
+oid sha256:acd0d78124ed6cce30a1a9504857343d946e3d30209469d5b4fa870def64e400
+size 24810
