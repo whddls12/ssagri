1f126b96598991a573fd9973d70b9d2e554d2bcd
황종인 <ghkdwhddls12@daum.net>
2023-09-25 09:22:26 +0900

    save
    Former-commit-id: 2d7aca16cb118031953a51797e87dc1f71f36fd2

--- frontend/src/components/loginPage.styles.tsx
+++ frontend/src/components/loginPage.styles.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b2402ddc3d3220a518b4b164f1c5d71acc6deadd21030438e4f69828fb0c93b5
-size 25294
+oid sha256:603c266e90fc4922132e5e2546d37b82afbb015b30c887a2b137e2346c3f4ee5
+size 25677
