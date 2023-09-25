729fe2c856a33c7847d3c4e8eba4565698960bb6
유태영 <tybam@naver.com>
2023-09-25 20:27:44 +0900

    Feat: auctionbid
    Former-commit-id: 0d33c0150f8818d95b6dcc36dfe6f12188f6da23

--- backend/ssagri/src/main/java/com/ssafy/ssagri/entity/auction/AuctionBid.java
+++ backend/ssagri/src/main/java/com/ssafy/ssagri/entity/auction/AuctionBid.java
@@ -1,2 +1,3 @@
-package com.ssafy.ssagri.entity.auction;public class AuctionBid {
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:b49bedd1ceabc0905465d330fb27113ccfe48bb0387bed037ca027a0126247db
+size 1047
