de1c11726da1cde9682232f61f69d1a1fb7299dc
황종인 <ghkdwhddls12@daum.net>
2023-09-25 12:45:44 +0900

    feat: 이메일 인증 기능 구현
    Former-commit-id: 4378605322cbc6ac8a54b2b1f7a087431a5cb831

--- frontend/src/components/loginPage.styles.tsx
+++ frontend/src/components/loginPage.styles.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:acd0d78124ed6cce30a1a9504857343d946e3d30209469d5b4fa870def64e400
-size 24810
+oid sha256:3c08f9b3676b80cc6bdf72104afeddb11da61cea04dd5b5bfbfdc192638700d0
+size 25142
