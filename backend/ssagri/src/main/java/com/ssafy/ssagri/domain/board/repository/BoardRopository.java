93bd782deac2a81b25d18dbd79f93520db563dc5
handaldog <handal@kakao.com>
2023-09-25 22:02:36 +0900

    feature: board api 생성
    Former-commit-id: 8db67e87593340ff870a2c836f5b7e40c90e5ace

--- backend/ssagri/src/main/java/com/ssafy/ssagri/domain/board/repository/BoardRopository.java
+++ backend/ssagri/src/main/java/com/ssafy/ssagri/domain/board/repository/BoardRopository.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:af2361757c46a9112bd326ad285a517d266c5f9a9fe5249b284d80f307b22089
-size 594
+oid sha256:60061a347611666234f83ee48e8adfb126eb45877a6113afed78b152bc7aedd8
+size 593
--- backend/ssagri/src/main/java/com/ssafy/ssagri/entity/comment/BoardComment.java
+++ backend/ssagri/src/main/java/com/ssafy/ssagri/entity/comment/BoardComment.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e35faea0335dab1a5bda8f28e10516b75dfbe36a3b69b725af5f30357ae65aea
-size 1608
+oid sha256:dad2ec7aedb81726bff9c64bab98fad0f6eb97074d89968636700a11568834ab
+size 1692
