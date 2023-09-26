31b6e348c6cc8f7193b1e5c1e6938e888f21a4ac
thdqudgns <thdqudgns9612@gmail.com>
2023-09-26 11:21:52 +0900

    Fix: 정합성 맞추기
    Former-commit-id: 4ee47d23cb8c386394d9d269e304c2ce16433908

--- frontend/src/components/loginPage.styles.tsx
+++ frontend/src/components/loginPage.styles.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:faee8e0abf5301f28f2eaec547e763badaa839fe92fcd34c2bc413ee41ac9dfb
+oid sha256:e3f0752c364c0f2c2d85b19d7dc3a36acd805b1a8a9e555d895447c20d2a1873
 size 25913
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:031a59486eac35be187db67a4b16c37c3ecd96fad293f7681324ddaaef76aa8f
-size 1199
+oid sha256:f555536be4b3313617c4e3c5293a00fff59f9846327991d8b3b03e45b077d379
+size 1330
