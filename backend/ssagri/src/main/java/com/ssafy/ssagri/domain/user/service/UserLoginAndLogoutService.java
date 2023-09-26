343575c4f2ca363ef0d63a265db4f81f73084811
신창학 <csg1353@naver.com>
2023-09-26 17:15:01 +0900

    feat : 카카오 전용 로그인 API 추가 및 redirecting local이 아니라 실제 주소로 이동
    Former-commit-id: 3991199358f3fdd39b1e818a3f437d47ddc5b1bb

--- backend/ssagri/src/main/java/com/ssafy/ssagri/domain/user/service/UserLoginAndLogoutService.java
+++ backend/ssagri/src/main/java/com/ssafy/ssagri/domain/user/service/UserLoginAndLogoutService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e0875aa1c2f1d765a409f14ba675f3dacc6530e356c02315cade0e6bddf5626c
-size 5594
+oid sha256:fcfbdb98d547d7167c91e49a6dc13f6a409016dfce526979ce8349cfa9fa841d
+size 6898
