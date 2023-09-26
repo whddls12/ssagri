ccbf4f22d7e4daee6e0fd62a7477c634f90928e6
송병훈 <thdqudgns9612@gmail.com>
2023-09-26 13:53:09 +0900

    Update loginPage.styles.tsx
    Former-commit-id: ba1417bc7245c93ac4ebe7a67285c48ed00bc193

--- frontend/src/components/loginPage.styles.tsx
+++ frontend/src/components/loginPage.styles.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d945fa22fc4c86d1d8c022818c9a2c9d0a745ff67ba87beb60a068277ac0eb64
-size 27365
+oid sha256:7e77c092ccb12a26261ff1be9fd9a370fe96432386ee0a40ca585076359e29d0
+size 27250
