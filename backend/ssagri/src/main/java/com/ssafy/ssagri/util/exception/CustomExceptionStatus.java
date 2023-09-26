65d294b1d19f12892ad94a716b8657144e0452e6
신창학 <csg1353@naver.com>
2023-09-26 15:49:01 +0900

    feat : 카카오 로그인 후 해당 액세스 토큰 정보 불러오기까지 완료
    Former-commit-id: 76e3f30bb1a6492f19373ce3c15a8a2dbf6108f9

--- backend/ssagri/src/main/java/com/ssafy/ssagri/util/exception/CustomExceptionStatus.java
+++ backend/ssagri/src/main/java/com/ssafy/ssagri/util/exception/CustomExceptionStatus.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:93dfa5a965f7afca04eceae389609c8f145ea37374dfd9405cdc05eaef0a871b
-size 3193
+oid sha256:b763d1caa2a3f670119646573585cfc865e49906db53f295b0d172cc9040f468
+size 3329
