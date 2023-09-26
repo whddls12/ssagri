de42087f0e780176bf56a181525951b69e8ea7f6
황종인 <ghkdwhddls12@daum.net>
2023-09-26 09:31:17 +0900

    feat: 회원가입시 프로필 사진 등록 기능 수정중
    Former-commit-id: d177b66d3780807064a6c8235408f9c225ac742c

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3902261c0d3c9ee49d680990bb1492d2935fa5373cfff047950a80163da017b9
+oid sha256:24a6da45f782692b337db36b38e718f1fe89e7d96966dafe23c8162f74bbdfcf
 size 767
