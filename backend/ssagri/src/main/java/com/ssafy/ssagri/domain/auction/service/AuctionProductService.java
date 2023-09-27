fd7dc1b9ef153b821045a0f2ad0528a27f66a72b
handaldog <handal@kakao.com>
2023-09-27 12:40:13 +0900

    feature : 보드, 경매 수벙
    Former-commit-id: 17c58640ca70ef821ff074ab2f9ac4c1e006f685

--- backend/ssagri/src/main/java/com/ssafy/ssagri/domain/auction/service/AuctionProductService.java
+++ backend/ssagri/src/main/java/com/ssafy/ssagri/domain/auction/service/AuctionProductService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0902f96ec0b31aa3c26469abd79071336482c98de2dff3fe1721c69448e38233
-size 9573
+oid sha256:05701fe67ffcb0aea4214a482e64c202e20771f6a08225a499ec5eeb38f3f271
+size 9757
--- backend/ssagri/src/main/java/com/ssafy/ssagri/domain/board/dto/BoardListDto.java
+++ backend/ssagri/src/main/java/com/ssafy/ssagri/domain/board/dto/BoardListDto.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:03ecbb94076b5afd957cf01e4b3cd73f9b4719ba74fe9f93515618219928fdf8
-size 628
+oid sha256:f1ca8e8e1a1da19b9175b50dfde970e43b177e8d5207d0481b02a9f0d8a10dea
+size 842
