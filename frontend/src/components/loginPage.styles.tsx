c702e108d5b7674358a278326d375e733d6f8f8b
joung <15gpfk@naver.com>
2023-09-27 09:34:44 +0900

    Fix: 로그인오류수정
    Former-commit-id: 62713bfb8c1e880b48456041adc3d9f9abf563d4

--- frontend/src/components/loginPage.styles.tsx
+++ frontend/src/components/loginPage.styles.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:193c3db9d09b714b3c9556d2338cb2e827099074eae5178b5855e60012e49f9e
-size 26689
+oid sha256:e1bd56f1e53c86fe9f1fc8c8402e2ab4364ba627eb3b296ee50d43934ec9f7c2
+size 26714
