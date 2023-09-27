5d079a54bf3687c24e1dbff0d35c7f0dd6db7039
handaldog <handal@kakao.com>
2023-09-27 14:55:46 +0900

    feature: s3 수정
    Former-commit-id: 7bca26a423ac43dfda38fd683d0ed85e5f17aefe

--- backend/ssagri/src/main/java/com/ssafy/ssagri/domain/auction/service/AuctionProductService.java
+++ backend/ssagri/src/main/java/com/ssafy/ssagri/domain/auction/service/AuctionProductService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0902f96ec0b31aa3c26469abd79071336482c98de2dff3fe1721c69448e38233
-size 9573
+oid sha256:50137acee75f849100fece3f580cb62b7be45e1abf2c27566bffc622b5f39c39
+size 7725
