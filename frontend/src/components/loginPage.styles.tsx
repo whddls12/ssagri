84f6f5b478b16a1facb0147ab5337b66284467a2
황종인 <ghkdwhddls12@daum.net>
2023-10-02 22:56:31 +0900

    Feat: 카카오 계정으로 회원가입 시 자동으로 회원가입 창으로 바뀌도록 수정
    Former-commit-id: baf5d315f138d4a89ca862de6f0f23612f07d3c7

--- frontend/src/components/loginPage.styles.tsx
+++ frontend/src/components/loginPage.styles.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ed652c8c63a9bd7d4c767ba7a6319a1e5ce5370c5ab00f74be4b433e7cebbc9d
-size 27998
+oid sha256:c37e35d642419b1b7be8b4c8719ecfed363dd0a026fe7f701e6403ac5cef574c
+size 28300
