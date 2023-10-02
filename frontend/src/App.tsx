b9e8a307b7a1b37e6b5e98ddfe081f0b3eedb642
황종인 <ghkdwhddls12@daum.net>
2023-10-02 22:31:16 +0900

    Feat: 소셜 로그인 구현중
    Former-commit-id: d78260c62dc4e4c93972620c50fbadf44a8f1983

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9fcc4452471d6b79a6339ceee7ef988c3c8e03f96f3942930ddea19ddc3e3d5c
-size 3486
+oid sha256:f1797b31e8125a22f54062defd6ce06a0c2847207ab3c3b1ad0370e97d7ae47c
+size 3656
--- frontend/src/components/loginPage.styles.tsx
+++ frontend/src/components/loginPage.styles.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4575056dc6deb35ce0413b247d8cd2f4677d1c3db282844f395fcf8864b0a300
-size 26575
+oid sha256:ed652c8c63a9bd7d4c767ba7a6319a1e5ce5370c5ab00f74be4b433e7cebbc9d
+size 27998
