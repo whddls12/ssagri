f509ff7339065e14a7cb08986835398ec4cb0aab
황종인 <ghkdwhddls12@daum.net>
2023-10-03 00:47:55 +0900

    fix: 빌드 에러 수정
    Former-commit-id: 02df566386836c599d5df48194e6e00c1ac2d077

--- frontend/src/components/loginPage.styles.tsx
+++ frontend/src/components/loginPage.styles.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:39a005dfb05c2611f6bd9e48c96c8a134d305fd11e1607c54527d0a91322effa
-size 28376
+oid sha256:b08d4a9a52c14e977a0d0bb776c02e903f301768bbbe1748552ac6342f3df3bb
+size 28375
