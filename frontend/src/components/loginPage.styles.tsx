d6303eafca0c4adb0cc769ec8d90ad35dd18d504
황종인 <ghkdwhddls12@daum.net>
2023-10-03 15:00:44 +0900

    axios baseURL 서버로 변경
    Former-commit-id: cf0db4e8bc26a93483af2eb8368144822efda116

--- frontend/src/components/loginPage.styles.tsx
+++ frontend/src/components/loginPage.styles.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b08d4a9a52c14e977a0d0bb776c02e903f301768bbbe1748552ac6342f3df3bb
-size 28375
+oid sha256:17506ac2a8de5a6ac2367bcc8816f6e0e7119a19bba81a14079966574b2fa24c
+size 28673
--- frontend/src/pages/SocialKakao.tsx
+++ frontend/src/pages/SocialKakao.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a401a085653c78b69fa31a7e301dd3e2c7b600e2bc0a1870a3f8bc0a9672521f
-size 2070
+oid sha256:a80a05709d3d691f32efe1e2bd09b7843e1ce80ebee5be41ee559526da936656
+size 2138
