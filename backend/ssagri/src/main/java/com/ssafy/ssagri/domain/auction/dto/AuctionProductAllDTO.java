72b7fa79e8e65cbc142eba1414ab33a7aa9aa741
handaldog <handal@kakao.com>
2023-10-04 10:38:50 +0900

    fix : 경매 상품 수정
    Former-commit-id: e12a82270e71294d353a5279e5c6bb1570ef124b

--- backend/ssagri/src/main/java/com/ssafy/ssagri/domain/auction/dto/AuctionProductAllDTO.java
+++ backend/ssagri/src/main/java/com/ssafy/ssagri/domain/auction/dto/AuctionProductAllDTO.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c5ec4c9cf4ea04fab71b6e99bb6b5b239b172326aad85461dbad071fe4923910
-size 1371
+oid sha256:d368521fa8eb074c6f45ed30d587393df41d2a61cfac3d7c77f8741a74c92c19
+size 1654
