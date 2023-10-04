a753b0f94c748bfd77696d894394518b6239ad76
유혜빈 <handal@kakao.com>
2023-10-04 17:39:24 +0900

    Update AuctionProductService.java
    Former-commit-id: a8e43e70f7bc59c3f4ee9af4a5f4f375dadde817

--- backend/ssagri/src/main/java/com/ssafy/ssagri/domain/auction/service/AuctionProductService.java
+++ backend/ssagri/src/main/java/com/ssafy/ssagri/domain/auction/service/AuctionProductService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d49286e6094a095c5faf10fd9833feef91a210bc51acf071fad54a0ff80d6a66
-size 12046
+oid sha256:ed9fbe71284ff6c801f661f003b6679b5aaed82fc978798974cd089e84314df6
+size 12040
