1524b6ac17e56f7abbac60566f1e12d2c9e2ac3d
유태영 <tybam@naver.com>
2023-10-04 17:31:09 +0900

    Fix: notificationservice fix
    Former-commit-id: 5877b7f79430e8b7aa343776b93919919a7bbe6e

--- backend/ssagri/src/main/java/com/ssafy/ssagri/domain/notification/NotificationService.java
+++ backend/ssagri/src/main/java/com/ssafy/ssagri/domain/notification/NotificationService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ba047f780f4e2bd30be332d306260c25032f2adae73c0d4cdcfce7c879398d2a
-size 4507
+oid sha256:87875e532d5518c6be763570d2c7a7cdd188d2d06064c6cb54b411ab7dfc1355
+size 4475
