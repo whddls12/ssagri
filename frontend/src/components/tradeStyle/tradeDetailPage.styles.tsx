93d0346cde7a950d71c394c27e7d23655fe57b59
thdqudgns <thdqudgns9612@gmail.com>
2023-10-04 09:42:06 +0900

    Fix: 중고거래 생성 및 디테일 수정
    Former-commit-id: 6aaae1ff195cd6ae0df6ddaf102f0ae2e3e5618b

--- frontend/src/components/tradeStyle/tradeDetailPage.styles.tsx
+++ frontend/src/components/tradeStyle/tradeDetailPage.styles.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:11dd15e5160a920626f93bc73c958d02c2419f736c71004f1dcccc5a13cd0064
-size 22475
+oid sha256:9c729ef79bfa0ab0a4b4a874dd60e915a7777c85d987267cd57b93f1cd6ee8bb
+size 22481
