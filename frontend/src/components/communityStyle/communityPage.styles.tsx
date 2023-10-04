7a7de84b4f475a9c77bbdfd47f9db73ac7ecbbcc
joung <15gpfk@naver.com>
2023-10-04 17:05:25 +0900

    Feat: 알림창 생성
    Former-commit-id: 7796af64a63b504222b8918d884f9b4b02620732

--- frontend/src/components/communityStyle/communityPage.styles.tsx
+++ frontend/src/components/communityStyle/communityPage.styles.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7bbe23199d582884e637a3cd99375df8fa1698128653efeadec6cab820749a06
-size 13388
+oid sha256:71bb21a417a8cc9b6873808eabfda9367b866282777650b7b2795891d1835de5
+size 14773
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:75038f45e848bbb302061865d850aab9019815b9dd82b4e5dc42c26d2839655a
-size 1116
+oid sha256:2ba6215f517a39167f57f537b80bffaf02421113e8de011d11098baf5c55f1d4
+size 1172
