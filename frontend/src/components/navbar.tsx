953efd011e656de6af4ffdb2114c449579cb1e92
joung <15gpfk@naver.com>
2023-10-04 17:57:42 +0900

    Fix: 알림 오류수정
    Former-commit-id: 41143cb8912c3cffa5f006406de10c590f25ebbc

--- frontend/src/components/navbar.tsx
+++ frontend/src/components/navbar.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d4878cb3b8d6f53b4404a538d87e280268122ee8d642a0d9bd54acaab99234d9
-size 7945
+oid sha256:2e94436e24b1bd234a496db342735b70ea78c52266cfc81f8c2da83103162ac7
+size 8022
