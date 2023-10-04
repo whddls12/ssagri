a54e9dd0f62342fe86d571aa84f595bff1999310
유태영 <tybam@naver.com>
2023-10-04 16:50:53 +0900

    Fix: auctionbid fix
    Former-commit-id: 50eeb5717ec44da591196e844b19c11073075a15

--- backend/ssagri/src/main/java/com/ssafy/ssagri/domain/auctionbid/service/AuctionBidService.java
+++ backend/ssagri/src/main/java/com/ssafy/ssagri/domain/auctionbid/service/AuctionBidService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b8c8054b7e90712d306255db8a0ace5e7942bfc3ebc403b757dc286f34a41381
-size 4447
+oid sha256:47db7a9b7299dd14b0686e966adbf19766f6c5f48bc691da01a3b9b085345cc2
+size 4450
