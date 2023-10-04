fbadee911cb1f46cc41e6565135704432155a6c3
thdqudgns <thdqudgns9612@gmail.com>
2023-10-04 10:33:46 +0900

    Fix: 동영상 처리 완료
    Former-commit-id: e1bee3d963b2bf7867851e397bb0dec390450b76

--- frontend/src/components/tradeStyle/tradeCreate.styles.tsx
+++ frontend/src/components/tradeStyle/tradeCreate.styles.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0d5ab7942b124e229bff7c787cdf8fc2ad3ea213b4f88006b4954363d1cc52e5
-size 15506
+oid sha256:1cf580529d1eb27e946d11604709fbbf47fb8a3a8cbbeb415507f5183c0dd021
+size 15707
