913898d8e9882f9402485562392d86ca94f9651f
황종인 <ghkdwhddls12@daum.net>
2023-10-04 16:54:17 +0900

    fix: ts error 수정
    Former-commit-id: d2981ac5e1723a4d5ccb88939c0e47470b76ccd9

--- frontend/src/components/auctionStyle/auctionDetailPage.styles.tsx
+++ frontend/src/components/auctionStyle/auctionDetailPage.styles.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5c4428cc54430ce3dca4960b98defe20ea4093178ddae07c9f08de3a89f58d06
-size 9770
+oid sha256:10a106cd2c8d104140fddbf80e2f9781f5eb1c3cd89a33cd0bf25e63c2942e0b
+size 9777
