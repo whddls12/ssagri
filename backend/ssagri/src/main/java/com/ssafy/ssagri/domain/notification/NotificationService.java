88babd2e80f1cab4653c850264c3b901eb900b7d
유태영 <tybam@naver.com>
2023-10-04 15:29:00 +0900

    Fix: auctionbid
    Former-commit-id: 90533febb00b9bdf424b6fa16e1c743beb5d4fb5

--- backend/ssagri/src/main/java/com/ssafy/ssagri/domain/notification/NotificationService.java
+++ backend/ssagri/src/main/java/com/ssafy/ssagri/domain/notification/NotificationService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3b65c379e16bc3b8371af2dc2c38f0d9abc697285cc27edf9c1bbb71606ef6c5
-size 4059
+oid sha256:ca75d55cfc052c20ed2c0a315ec2ae746be3d08a503affc410d6b30dbc584fa9
+size 4141
