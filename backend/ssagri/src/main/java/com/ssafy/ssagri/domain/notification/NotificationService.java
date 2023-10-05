46e3fec4909b45f8f90d12b198d0ed8f8537c983
유태영 <tybam@naver.com>
2023-10-05 21:08:27 +0900

    Fix: notification log.info
    Former-commit-id: ecf66a3e8c3c53ffb700cbdf1577412536ddcf34

--- backend/ssagri/src/main/java/com/ssafy/ssagri/domain/notification/NotificationService.java
+++ backend/ssagri/src/main/java/com/ssafy/ssagri/domain/notification/NotificationService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c9020db63daa6094954c2c527aa0daa03ce7631751d9f5443ddac788253cbe45
-size 5675
+oid sha256:c19263d575abde06fae47a5ea4a56629d8d4bbbfd96d0790aca9e73c353a7c88
+size 5805
