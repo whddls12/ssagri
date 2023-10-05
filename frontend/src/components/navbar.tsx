9311acfb05d2012083c518d0b97935863c46ed67
joung <15gpfk@naver.com>
2023-10-05 15:05:02 +0900

    Feat: 알람 변수 변경
    Former-commit-id: e28655e8c187bc75cac916dbff33bb02165d0b63

--- frontend/src/components/navbar.tsx
+++ frontend/src/components/navbar.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b9ff778f03d6baa2c35bb9c63a4b0928fb1c87b78251ee38e10b4057fb73bea6
-size 8636
+oid sha256:36355ad71ac4107e332c0f7076241e310541f2cd959f5c8e7c04000600c847ac
+size 8637
