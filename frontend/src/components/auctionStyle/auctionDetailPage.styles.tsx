f1b060c7537bcc9638dadec383ec002dbe837cfd
황종인 <ghkdwhddls12@daum.net>
2023-10-05 23:55:51 +0900

    save
    Former-commit-id: 8279146325acde9b427db973a733cc415d9425ba

--- frontend/src/components/auctionStyle/auctionDetailPage.styles.tsx
+++ frontend/src/components/auctionStyle/auctionDetailPage.styles.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f82bd55f47fe41b15136eff0c55e90a7018a223e792ed56615951f04de070a52
-size 19635
+oid sha256:e562c7658facb66572330a58a17b787eb09a02d2a890baea4dfb344534ef7e7b
+size 19647
