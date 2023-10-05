4e50908da8b6f8411880aa32fe2b7d9a84d84fb2
신창학 <csg1353@naver.com>
2023-10-05 10:24:04 +0900

    fix : Endpoint 명칭 변경
    Former-commit-id: a67fed5924440d561ff9dcb5880c0adc9d62d5e4

--- backend/ssagri/src/main/java/com/ssafy/ssagri/domain/user/controller/UserMypageController.java
+++ backend/ssagri/src/main/java/com/ssafy/ssagri/domain/user/controller/UserMypageController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e2cdbcc766eb839b3e48fc378897f16c6989b419a706093870354875c2592d9c
-size 2696
+oid sha256:8be7bfe1b03f0f6cdcd3548bd85cc8153800c6f65fb5b4011b75c20ba4f386df
+size 2710
