cde3a5790f8bbc721e156175db35c7ea82d11fd1
유태영 <tybam@naver.com>
2023-10-05 15:19:22 +0900

    Feat: chat notification
    Former-commit-id: 81d856e2f0049fc54589142526481bd4edea87fd

--- backend/ssagri/src/main/java/com/ssafy/ssagri/domain/notification/NotificationService.java
+++ backend/ssagri/src/main/java/com/ssafy/ssagri/domain/notification/NotificationService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fd9166cb4e052e135fcd6ebf2129932d7237a05f088a354c1be41e784c5ea730
-size 4489
+oid sha256:c9020db63daa6094954c2c527aa0daa03ce7631751d9f5443ddac788253cbe45
+size 5675
