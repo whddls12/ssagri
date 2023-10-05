b43c443ade665a5ca579725c9563d93a5818c74a
handaldog <handal@kakao.com>
2023-10-06 02:57:25 +0900

    fix : 보드 수정
    Former-commit-id: 61444fe008db523b1fc54654de780c563b2f528c

--- backend/ssagri/src/main/java/com/ssafy/ssagri/domain/board/service/BoardService.java
+++ backend/ssagri/src/main/java/com/ssafy/ssagri/domain/board/service/BoardService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:16244bcbe59ff9178d7c91ba25e2b2661262483b01cdd7fc6210d91a2c9c2d28
-size 12174
+oid sha256:a576c3a03c1c42e1fa3f0b07079462942e99fe723ad90ca3d064046e54293d9d
+size 12184
