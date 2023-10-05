3ff19f34f985f6f0ede2f6ea4377f8dfba23af48
thdqudgns <thdqudgns9612@gmail.com>
2023-10-05 21:11:29 +0900

    Fix: 채팅 수정 완료
    Former-commit-id: 4ec67ca4c14c66b3117f29fd40f1363106cb1662

--- backend/ssagri/src/main/java/com/ssafy/ssagri/domain/chatroom/repository/ChatRoomCustomRepositoryImpl.java
+++ backend/ssagri/src/main/java/com/ssafy/ssagri/domain/chatroom/repository/ChatRoomCustomRepositoryImpl.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c43f447da1917ad15149b1e2251591b045828cfc3c30fd9c3ba60e1806a3ab36
-size 2951
+oid sha256:05670a3c39acc3f56d18de127618a163bd7acdd5c703d438566a77658246f333
+size 3045
