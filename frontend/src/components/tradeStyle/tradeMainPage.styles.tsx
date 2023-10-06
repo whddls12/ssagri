ca0c71ec42de8f6ca0debc3e31ef84f18a8e6a10
joung <15gpfk@naver.com>
2023-10-06 09:37:16 +0900

    Feat: 거래 페이지 위치수정
    Former-commit-id: b1465e54eaddf6401417c4c77c64c26140572867

--- frontend/src/components/tradeStyle/tradeMainPage.styles.tsx
+++ frontend/src/components/tradeStyle/tradeMainPage.styles.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4e2abc1e5231dabc242afacc923b099922bfdeb3ba27e7ade031d171f969670f
-size 22125
+oid sha256:0d8adc27324578d1c9c0f186bc233e0738aed992b9ea3e08b5d00fb10c35e5ce
+size 22187
