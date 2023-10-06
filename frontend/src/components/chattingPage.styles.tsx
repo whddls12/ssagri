727ef7ab39d198a25f6ad193490dfb3bff083a44
thdqudgns <thdqudgns9612@gmail.com>
2023-10-06 09:51:58 +0900

    Fix: 이상하지만, 시간 수정
    Former-commit-id: 7b1afc556b74e3b54c4bdd6209518b185db7570b

--- frontend/src/components/chattingPage.styles.tsx
+++ frontend/src/components/chattingPage.styles.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3d51bb753a096ff4990278c9bad615c1b8d881290b40042c3613c6dfac93c4c6
-size 27061
+oid sha256:3166517d193efc6893fcb02a5198c0585eb5db174064863bba26f0891cbfa771
+size 27263
