0c53e891954ff93c8abb00cb36a1c916cdbc1e41
thdqudgns <thdqudgns9612@gmail.com>
2023-10-06 10:05:30 +0900

    Fix: 시간 수정 완
    Former-commit-id: 4ccb93c450eb42e949ca932e400897f6731c4616

--- frontend/src/components/chattingPage.styles.tsx
+++ frontend/src/components/chattingPage.styles.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:efe9f49205539c88a8526edafe06efe44d566285f0ad40ccf70a05603f0e0e99
-size 26961
+oid sha256:38cacdaa6688210eacf8eb5bce02ee3369dc4fc76c5da863c82d3d9e37bf48a0
+size 26763
