d0e39605a912c483a0de009cb83b9b961b3cef4a
joung <15gpfk@naver.com>
2023-10-06 09:18:33 +0900

    Feat: 메인,거래페이지 수정
    Former-commit-id: a3efc1c8f20fc726178a4cfc170022bf849c03fd

--- frontend/src/components/mainStyle/mainPage.styles.tsx
+++ frontend/src/components/mainStyle/mainPage.styles.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4777f6a12ee4b6ea7b6cdfce0e9329b42e9dc80738396512e7760171a75d2e42
-size 48637
+oid sha256:d6d3657a94dceb7bab871ae718b5a4497dc024c0369daef09213b72f8ad030c5
+size 48770
--- frontend/src/components/tradeStyle/tradeMainPage.styles.tsx
+++ frontend/src/components/tradeStyle/tradeMainPage.styles.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:63841d2c380c49caf723baac9d907cb13d2ff6627850ad1f0164d24056364f3f
-size 22105
+oid sha256:4e2abc1e5231dabc242afacc923b099922bfdeb3ba27e7ade031d171f969670f
+size 22125
