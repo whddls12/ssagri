a2797b2c61bfd90e39896af432a0f72ad2313701
석정원 <15gpfk@naver.com>
2023-10-06 09:39:11 +0900

    Update tradeMainPage.styles.tsx
    Former-commit-id: f60e14307566df4810c369668e01bed55175c77e

--- frontend/src/components/tradeStyle/tradeMainPage.styles.tsx
+++ frontend/src/components/tradeStyle/tradeMainPage.styles.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0d8adc27324578d1c9c0f186bc233e0738aed992b9ea3e08b5d00fb10c35e5ce
-size 22187
+oid sha256:3b165287bc1c877d90bee217ec3c9deaf3c6f51bdbfd7690539bff3fb43ebaa2
+size 22188
