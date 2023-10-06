7657a7bdd9502a75d36228f514e7232e1a82037a
석정원 <15gpfk@naver.com>
2023-10-06 11:16:40 +0900

    Update communityPage.styles.tsx
    Former-commit-id: b9623720ebbe9808fe521e2a9e3f91fd2d6c8ca1

--- frontend/src/components/communityStyle/communityPage.styles.tsx
+++ frontend/src/components/communityStyle/communityPage.styles.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6be0d8a4c461016968b10f25d0a4090c0c55687826dcb6a17f2e0078d5ea57af
-size 19145
+oid sha256:abd31745a2d7fbd85c45520339560015f405d31ff70f7d1a289e22ed8ea06c53
+size 19247
